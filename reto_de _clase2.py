370ec779031da38fec8a7019d2f945ef362b8581
matias1lol <matias1lol@users.noreply.github.com>
2023-05-23 15:33:50 -0400

    lab3

--- reto_de _clase2.py
+++ reto_de _clase2.py
@@ -1,23 +1,14 @@
-# Solicitar los nombres, pesos, estaturas y edades de los pacientes
 nombres = []
 pesos = []
 estaturas = []
 edades = []
-
 for i in range(3):
-    # Pedir el nombre del paciente
     nombre = input(f"Ingrese el nombre del paciente {i+1}: ")
     nombres.append(nombre)
-
-    # Pedir el peso del paciente
     peso = float(input(f"Ingrese el peso del paciente {i+1} en kg: "))
     pesos.append(peso)
-
-    # Pedir la estatura del paciente
     estatura = float(input(f"Ingrese la estatura del paciente {i+1} en metros: "))
     estaturas.append(estatura)
-
-    # Pedir la edad del paciente
     while True:
         try:
             edad = int(input(f"Ingrese la edad del paciente {i+1}: "))
@@ -28,14 +19,10 @@ for i in range(3):
                 break
         except ValueError:
             print("Error: la edad debe ser un número entero")
-
-# Guardar la información en tuplas
 pacientes = []
 for i in range(3):
     paciente = (nombres[i], pesos[i], estaturas[i], edades[i])
     pacientes.append(paciente)
-
-# Mostrar la información de los pacientes
 for i, paciente in enumerate(pacientes):
     print(f"Paciente {i+1}:")
     print(f"Nombre: {paciente[0]}")
---
+++ reto_de_clase6.py
@@ -0,0 +1,4 @@
+suma = 1
+for numero in range(10, 31, 2):
+    suma += numero
+print("La suma de los números obtenidos es:", suma)
